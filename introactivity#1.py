58a6922810456831b19549ba5dee2b9af3df3bbe
ksolera <79610570+ksolera@users.noreply.github.com>
2022-08-25 09:37:28 -0400

    intro problems 1

--- introactivity#1.py
+++ introactivity#1.py
@@ -1,6 +1,6 @@
 def string_both_ends(str):
     if len(str) < 2:
-        return ''
+        return 'String is too short!'
     return str[0:2] + str[-2:]
 
 string = input("Enter your String:")
---
+++ introactivity#2.py
@@ -0,0 +1,12 @@
+def numToReturn(numStart, numEnd):
+    x = range(numStart,numEnd, 7)
+    for n in x:
+        print(n)
+
+
+
+numStart = input("Please enter first number: ")
+numEnd = input("Please enter last number: ")
+finalRange = numToReturn
+
+
