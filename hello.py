4ecbbee2e281fbfdf68b4f97870099c460c11336
ksolera <79610570+ksolera@users.noreply.github.com>
2022-08-29 12:46:52 -0400

    calculator try

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+print("Hello, World, Weight Calculator!")
+weight = int(input("Weight: "))
+unit = input("(K)g or (L)bs: ")
+if unit.upper() == "K":
+    converted = weight / .45
+    print("Weight in Lbs: " + str(converted))
+else:
+    converted = weight * .45
+    print("Weight in Kgs: " + str(converted))
+
---
+++ introactivity#4.py
@@ -0,0 +1,1 @@
+items = int(input("Number of items: "))
