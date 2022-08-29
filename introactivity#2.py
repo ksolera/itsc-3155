296ae8d9b5292a49b0878f391cc74c822e70f0b4
ksolera <79610570+ksolera@users.noreply.github.com>
2022-08-29 11:42:23 -0400

    problems

--- introactivity#2.py
+++ introactivity#2.py
@@ -1,12 +1,10 @@
-def numToReturn(numStart, numEnd):
-    x = range(numStart,numEnd, 7)
-    for n in x:
-        print(n)
+first = int(input("Please enter first number: "))
+second = int(input("Please enter second number: "))
+result = ""
+for i in range(first,second+1):
+        if i%7==0 and i%5!=0:
+            result += str(i)+","
 
-
-
-numStart = input("Please enter first number: ")
-numEnd = input("Please enter last number: ")
-finalRange = numToReturn
-
+result =result[:-1]
+print(result)
 
