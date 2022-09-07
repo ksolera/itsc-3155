924de68302f7b17f4a80ec1c876c76cb636d9e24
ksolera <79610570+ksolera@users.noreply.github.com>
2022-09-06 22:38:23 -0400

    update

--- introactivity#4.py
+++ introactivity#4.py
@@ -1,3 +1,13 @@
-items = int(input("Number of items: "))
-price =int(input("Input item and price: "))
+items=int(input("Number of items :"))
+list=dict()
+for i in range(items):
+    take_input=input(("Input item and price :"))
+    temp=take_input.split(' ')
+
+list[temp[0]]=int(temp[1])
+
+sort_list=sorted(list.items(),key=lambda x:x[1],reverse=False)
+
+for i in sort_list:
+    print(i[0],i[1])
 
